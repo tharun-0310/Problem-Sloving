29eef4a12f3bce943263e745febd26195e1f6229
Tharun S <151508596+tharun-0310@users.noreply.github.com>
2025-02-24 15:58:54 +0530

    Add files via upload

---
+++ p5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Size
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+         int x=sc.nextInt();
+         int y=sc.nextInt();
+         
+         for(int i=0;i<y;i++)
+         {
+             System.out.println(x);
+         }
+         
+        
+    }
+}
---
+++ p6_multiples of 100.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Size
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+         int x=sc.nextInt();
+      
+         
+         for(int i=x;i<=1000;i=i+100)
+         {
+             System.out.println(i);
+         }
+         
+        
+    }
+}
