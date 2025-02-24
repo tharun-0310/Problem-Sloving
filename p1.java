c5e21134294604c6b267800ff5208ea74418a2ca
Tharun S <151508596+tharun-0310@users.noreply.github.com>
2025-02-24 14:54:36 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner (System.in);
+        int num=sc.nextInt();
+        String str=sc.next();
+        char ch=sc.next().charAt(0);
+        System.out.println(num);
+        System.out.println(str);
+        System.out.println(ch);
+    }
+}
+        
+    
